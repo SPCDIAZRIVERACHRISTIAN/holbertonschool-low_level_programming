a9eb3fe9d05f28ec4b26b80651300a8bcfe6b3c3
Christian Diaz <christianda3@gmail.com>
2024-02-23 11:42:21 -0400

    Added file 6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("char size: %lu bytes\n", sizeof(char));
+	printf("int size: %lu bytes\n", sizeof(int));
+	printf("long int size: %lu bytes\n", sizeof(long int));
+	printf("long long int size: %lu bytes\n", sizeof(long long int));
+	printf("float size: %lu bytes\n", sizeof(float));
+}
