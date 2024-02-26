9f316e4b6b634c956c5d8bc5207016aa60c83025
Christian <Christian>
2024-02-26 11:48:18 -0400

    fixed file 1 swap c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,10 +3,13 @@
 /**
  * void swap_int - swaps the value of a with the value of b.
  *
- * @n: the result is integer b in integer a
+ * @a: the result is integer b in integer a
+ * @b: will have the result from a in b.
  */
 
 void swap_int(int *a, int *b)
 {
-	*a = &b;
+	int c = *a;
+	*a = *b;
+	*b = c;
 }
