a8501ffec347335cd3615fc1b46a18a539049348
Christian <Christian>
2024-02-26 11:34:49 -0400

    Added file 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * void swap_int - swaps the value of a with the value of b.
+ *
+ * @n: the result is integer b in integer a
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+}
