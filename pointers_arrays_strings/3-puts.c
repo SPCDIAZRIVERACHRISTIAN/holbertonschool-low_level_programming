b9c295f947a132cbeca6a5685d16df15706bb22f
Christian <Christian>
2024-02-26 20:19:51 -0400

    fixed file 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,13 +3,9 @@
 /**
  * _puts - prints a string, followed by a new line.
  * @str: input string.
- * Return: no return.
  */
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
-	_putchar('\n');
+	while (*str)
+		_puts(*str);
 }
