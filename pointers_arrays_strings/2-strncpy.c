944358ae738ec97303b2e34a2438f2c97ce67cfe
Christian <Christian>
2024-02-28 13:23:12 -0400

    fixed file 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -3,12 +3,15 @@
 /**
  * _strncpy - copying a string
  * @dest: string where I want to copy
- * @src:string I want to copy
+ * @src:string I want to copyi
+ * Return: points to the end of string dest
+ * @n: number of bytes
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int alpha;
+
 	alpha = 0;
 
 	while (dest[alpha] != '\0' && alpha < n)
@@ -16,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[alpha] = src[alpha];
 		alpha++;
 	}
-	
+
 	while (alpha < n)
 	{
 		dest[alpha] = '\0';
