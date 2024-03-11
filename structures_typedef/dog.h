b0bf5da0e02e8a69c040cb1f6069f00bf7ddd5db
Christian <Christian>
2024-03-11 19:11:24 -0400

    fixed dog again

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,19 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog_t - the structure defines three types.
+ * struct dog - the structure defines three types.
  * @name: name of dog
  * @age: age of dog
  * @owner: who owns the dog
  */
 
-struct dog_t
+struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-void init_dog(struct dog_t *d, char *name, float age, char *owner);
-void print_dog(struct dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+{
+    /* dog_t - type def for structure */
+};
+
 
 #endif
