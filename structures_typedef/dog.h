0e10ca2e049ebf2562cec4e2c4d86e46e4997c1b
Christian <Christian>
2024-03-11 19:02:20 -0400

    updated dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - the structure defines three types.
+ * struct dog_t - the structure defines three types.
  * @name: name of dog
  * @age: age of dog
  * @owner: who owns the dog
  */
 
-struct dog
+struct dog_t
 {
 char *name;
 float age;
