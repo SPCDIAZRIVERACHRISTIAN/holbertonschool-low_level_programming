2cd6c5e7524f29fd2aa137066a4430a13a9ebd26
Christian <Christian diaz>
2024-03-11 21:05:54 -0400

    added file free dog and updated dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,5 +17,6 @@ typedef struct dog dog_t;
     /* dog_t - type def for structure */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
