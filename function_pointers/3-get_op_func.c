940d248032081a33392d2c70d1f2b9e3b970c709
Christian <Christian diaz>
2024-03-13 11:03:39 -0400

    updated calc header get op func and op func for calculator task

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,10 +1,34 @@
 #include "3-calc.h"
-op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+
+/**
+ * get_op_func - fetches the operator requested
+ * by the user
+ * @s: points to the operator entered by user
+ * 
+ * Return: points to the integer result
+ * of the math op
+*/
+
+int (*get_op_func(char *s))(int, int)
+{   
+    op_t ops[] = {
+            {"+", op_add},
+            {"-", op_sub},
+            {"*", op_mul},
+            {"/", op_div},
+         {"%", op_mod},
+         {NULL, NULL}
+        };
+        int i;
+
+        i = 0;
+        //only one while and if
+    while (ops.op[i])
+        {
+            if (strcmp(ops.op[i], s) == 0)
+            {
+                return(ops[i].f);
+            }
+        }
+    return (NULL);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: first integer to be added
+ * @b: second integer to be added
+ * 
+ * Return: returns op to main
+*/
+
+int op_add(int a, int b)
+{
+    return (a+b);
+}
+/**
+ * op_sub - substracts two integers
+ * @a: first integer to be subtracted
+ * @b: second integer to be subtracted
+ * 
+ * Return: returns op to main
+*/
+int op_sub(int a, int b)
+{
+    return (a-b);
+}
+/**
+ * op_mul - multiplies two integers
+ * @a: first integer to be multiplied
+ * @b: second integer to be multiplied
+ * 
+ * Return: returns op to main
+*/
+int op_mul(int a, int b)
+{
+    return (a*b);
+}
+/**
+ * op_div - divides two integers
+ * @a: first integer to be divided
+ * @b: second integer to be divided
+ * 
+ * Return: returns op to main
+*/
+int op_div(int a, int b)
+{
+    return (a/b);
+}
+/**
+ * op_mod - divides the numerator
+ * between the denominator of integers
+ * @a: first integer to be modulated
+ * @b: second integer to be modulated
+ * 
+ * Return: returns op to main
+*/
+int op_mod(int a, int b)
+{
+    return (a%b);
+}
