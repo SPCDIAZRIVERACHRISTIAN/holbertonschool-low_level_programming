b6dd66bdf7aaad73195fcf2ab3c352abf523ed4e
Christian <Christian>
2024-03-13 20:29:37 -0400

    fixed calc

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,36 +1,37 @@
 #include "3-calc.h"
 
 /**
- * 
- * 
- * 
- * 
+ * main - function that calls operator to do math
+ * @argc: argument counter
+ * @argv: argument string vector
+ *
+ * Return: succesful if 0
 */
 
 int main(int argc, char *argv[])
 {
-    int c, d, res;
-    int (*fun)(int, int);
+	int c, d, res;
+	int (*fun)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    c = atoi(argv[1]);
-    d = atoi(argv[3]);
-    fun = get_op_func(argv[2]);
+	c = atoi(argv[1]);
+	d = atoi(argv[3]);
+	fun = get_op_func(argv[2]);
 
-    if (!fun)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (!fun)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    res = fun(c, d);
-    
-    printf("%d\n", res);
+	res = fun(c, d);
 
-    return (0);
+	printf("%d\n", res);
+
+	return (0);
 }
