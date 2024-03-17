9de22413305e9564be1125bd1fd41d1ec852d9c5
Christian <Christian>
2024-03-16 23:56:02 -0400

    Fixed 2 print name to do what is intended

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -4,7 +4,7 @@
  * print_strings - prints strings
  * @n: number of strings
  * @separator: points to strings printed between strings
- *
+ * estabas haciendo un string para poder asignarlo a va_arg y eso iba a reflejar el string en n
 */
 
 void print_strings(const char *separator, const unsigned int n, ...)
@@ -18,12 +18,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (c = 0; c < n; c++)
 	{
+		st = va_arg(list, char *);
 		if (!separator)
-			printf("%s", va_arg(list, int));
-		else if (!n)
+			printf("%s", st);
+
+		if (!st)
 			printf("(nil)");
+
+		else if (separator && c == 0)
+			printf("%s", st);
+
 		else
-			printf("%s%s", separator, va_arg(list, int));
+			printf("%s%s", separator, st);
 	}
 	va_end(list);
 	printf("\n");
