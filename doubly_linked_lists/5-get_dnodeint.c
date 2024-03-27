9fe45de5ab793394fd92c9768354998a655ec4cc
Christian <Christian>
2024-03-27 14:43:57 -0400

    Added file 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,11 +13,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *node = head;
 	unsigned int i = 0;
 
-	if(!node)
-		return (NULL);
 
 	while (i < index)
 	{
+		if(!node)
+			return (NULL);
+			
 		node = node->next;
 		i++;
 	}
