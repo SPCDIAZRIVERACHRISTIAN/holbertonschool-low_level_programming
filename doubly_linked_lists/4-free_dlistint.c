a79dd31d458efb89b88c89f6ae93e87b9658f4ae
Christian <Christian>
2024-03-27 13:58:46 -0400

    Added file 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+        dlistint_t *temp;
+
+        while (head)
+        {
+                temp = head->next;
+                free(head);
+                head = temp;
+        }
+}
