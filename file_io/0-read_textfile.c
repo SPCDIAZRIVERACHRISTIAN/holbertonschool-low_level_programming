3051a1769437a753cdd7991b8ea155afe718757c
Christian <Christian>
2024-03-28 16:51:19 -0400

    fixed file 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,6 +13,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd, w, r;
 	char *wordc;
 
+	if (filename == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
@@ -23,6 +26,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	r = read(fd, wordc, letters);
 	w = write(STDOUT_FILENO, wordc, r);
+
+	if (w == -1 || w != r)
+		return (0);
 
 	free(wordc);
 	close(fd);
